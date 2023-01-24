bcfb8d283f0e024b16ee6e8a2601b248c83a4c8a
HLCaptain <pkblazsak@gmail.com>
2023-01-24 12:38:57 +0100

    Created a different tree when logging with Timber in release builds to prepare logging into Firebase Analytics.

--- app/src/main/java/illyan/jay/MainApplication.kt
+++ app/src/main/java/illyan/jay/MainApplication.kt
@@ -9,6 +9,14 @@ class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        Timber.plant(Timber.DebugTree())
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(object : Timber.Tree() {
+                override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                    // TODO: make custom logging for Firebase Analytics in release builds
+                }
+            })
+        }
     }
 }
