b8d7898f913e2784090659601f98f0ab93fbf42f
HLCaptain <pkblazsak@gmail.com>
2023-03-19 12:50:47 +0100

    Typo fix.

--- app/src/main/java/illyan/jay/util/Util.kt
+++ app/src/main/java/illyan/jay/util/Util.kt
@@ -211,7 +211,7 @@ fun Modifier.cardPlaceholder(
 }
 
 /**
- * Finds all potential URLs's start and end indices in the String.
+ * Finds all potential URLs' start and end indices in the String.
  */
 fun String.findUrlIntervals(
     urlRegex: Regex = "[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)".toRegex()
