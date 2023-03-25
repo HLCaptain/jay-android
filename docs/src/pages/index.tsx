9940deab0044c4a238d1cb78a1055bffb4a394c4
HLCaptain <pkblazsak@gmail.com>
2023-03-25 23:53:41 +0100

    Added missing files. Configured Features on landing page.

---
+++ docs/src/pages/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons + " --ifm-spacing-horizontal"}>
+          <Link
+            className="button button--primary button--lg"
+            to="/docs/intro">
+            Get Started with Jay 🐦
+          </Link>
+          <Link
+            className="button button--outline button--primary button--lg"
+            href="https://ko-fi.com/illyan">
+            Support Jay 💖
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
