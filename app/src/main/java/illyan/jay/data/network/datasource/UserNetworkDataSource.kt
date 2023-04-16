10413c9e4685ee0e13e9c899dbbb617a874b50f5
HLCaptain <pkblazsak@gmail.com>
2023-04-16 19:26:39 +0200

    Now using Flows to handle snapshots instead of EventListeners for getting User document.

--- app/src/main/java/illyan/jay/data/network/datasource/UserNetworkDataSource.kt
+++ app/src/main/java/illyan/jay/data/network/datasource/UserNetworkDataSource.kt
@@ -22,11 +22,10 @@ import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import com.google.firebase.firestore.DocumentSnapshot
-import com.google.firebase.firestore.EventListener
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.firestore.MetadataChanges
 import com.google.firebase.firestore.WriteBatch
+import com.google.firebase.firestore.ktx.snapshots
 import com.google.firebase.firestore.ktx.toObject
 import illyan.jay.data.DataStatus
 import illyan.jay.data.network.model.FirestoreUser
@@ -34,35 +33,37 @@ import illyan.jay.di.CoroutineScopeIO
 import illyan.jay.domain.interactor.AuthInteractor
 import illyan.jay.util.runBatch
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import timber.log.Timber
-import java.util.concurrent.Executors
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.coroutines.cancellation.CancellationException
 
 @Singleton
 class UserNetworkDataSource @Inject constructor(
     private val firestore: FirebaseFirestore,
     private val authInteractor: AuthInteractor,
     private val appLifecycle: Lifecycle,
-    @CoroutineScopeIO private val coroutineScopeIO: CoroutineScope
+    @CoroutineScopeIO private val coroutineScopeIO: CoroutineScope,
 ) : DefaultLifecycleObserver {
-    private val executor = Executors.newSingleThreadExecutor()
 
-    private val _userListenerRegistration = MutableStateFlow<ListenerRegistration?>(null)
+    private val _userListenerJob = MutableStateFlow<Job?>(null)
     private val _userReference = MutableStateFlow<DocumentSnapshot?>(null)
     private val _userStatus = MutableStateFlow(DataStatus<FirestoreUser>())
     private val _cloudUserStatus = MutableStateFlow(DataStatus<FirestoreUser>())
 
     val userStatus: StateFlow<DataStatus<FirestoreUser>> by lazy {
-        if (_userListenerRegistration.value == null && _userStatus.value.isLoading != true) {
+        if (_userListenerJob.value == null && _userStatus.value.isLoading != true) {
             Timber.d("User StateFlow requested, but listener registration is null, reloading it")
             refreshUser()
         }
@@ -74,7 +75,7 @@ class UserNetworkDataSource @Inject constructor(
     }.stateIn(coroutineScopeIO, SharingStarted.Eagerly, userStatus.value.data)
 
     val cloudUserStatus: StateFlow<DataStatus<FirestoreUser>> by lazy {
-        if (_userListenerRegistration.value == null && _cloudUserStatus.value.isLoading != true) {
+        if (_userListenerJob.value == null && _cloudUserStatus.value.isLoading != true) {
             Timber.d("User StateFlow requested, but listener registration is null, reloading it")
             refreshUser()
         }
@@ -107,8 +108,8 @@ class UserNetworkDataSource @Inject constructor(
 
     private fun resetUserListenerData() {
         Timber.v("Resetting user listener state")
-        _userListenerRegistration.value?.remove()
-        if (_userListenerRegistration.value != null) _userListenerRegistration.update { null }
+        _userListenerJob.value?.cancel(CancellationException("User listener reset requested, cancelling ongoing job"))
+        if (_userListenerJob.value != null) _userListenerJob.update { null }
         if (_userReference.value != null) _userReference.update { null }
         _userStatus.update { DataStatus() }
         _cloudUserStatus.update { DataStatus() }
@@ -129,7 +130,12 @@ class UserNetworkDataSource @Inject constructor(
 
     private fun refreshUser(
         userUUID: String = authInteractor.userUUID.toString(),
-        onError: (Exception) -> Unit = { Timber.e(it, "Error while getting user data: ${it.message}") },
+        onError: (Exception) -> Unit = {
+            Timber.e(
+                it,
+                "Error while getting user data: ${it.message}"
+            )
+        },
         onSuccess: (FirestoreUser?) -> Unit = {},
     ) {
         Timber.v("Refreshing user ${userUUID.take(4)} requested")
@@ -141,37 +147,36 @@ class UserNetworkDataSource @Inject constructor(
         _userStatus.update { it.copy(isLoading = true) }
         _cloudUserStatus.update { it.copy(isLoading = true) }
         Timber.d("Connecting snapshot listener to Firebase to get ${userUUID.take(4)} user's data")
-        val snapshotListener = EventListener<DocumentSnapshot> { snapshot, error ->
-            Timber.v("New snapshot regarding user ${userUUID.take(4)}")
-            if (error != null) {
-                onError(error)
-            } else {
-                val user = snapshot?.toObject<FirestoreUser>()
-                onSuccess(user)
-                if (snapshot != null) {
-                    // Update _userReference value with snapshot when snapshot is not null
-                    _userReference.update { snapshot }
-                } else if (_userReference.value != null) {
-                    // If snapshot is null, then _userReference is invalid if not null. Assign null to it.
-                    _userReference.update { null }
-                }
-                processUser(
-                    user = user,
-                    isFromCache = snapshot?.metadata?.isFromCache ?: false,
-                    userUUID = userUUID
-                )
+        _userListenerJob.value?.cancel(CancellationException("Refreshing user, launching new job to handle updates, old job is cancelled"))
+        _userListenerJob.update {
+            coroutineScopeIO.launch {
+                firestore
+                    .collection(FirestoreUser.CollectionName)
+                    .document(userUUID)
+                    .snapshots(MetadataChanges.INCLUDE).collectLatest { snapshot ->
+                        Timber.v("New snapshot regarding user ${userUUID.take(4)}")
+                        val user = snapshot.toObject<FirestoreUser>()
+                        onSuccess(user)
+                        // Update _userReference value with snapshot when snapshot is not null
+                        _userReference.update { snapshot }
+                        processUser(
+                            user = user,
+                            isFromCache = snapshot.metadata.isFromCache,
+                            userUUID = userUUID,
+                            updateCachedUserStatus = { status -> _userStatus.update { status } },
+                            updateCloudUserStatus = { status -> _cloudUserStatus.update { status } },
+                        )
+                    }
             }
         }
-        _userListenerRegistration.value = firestore
-            .collection(FirestoreUser.CollectionName)
-            .document(userUUID)
-            .addSnapshotListener(executor, MetadataChanges.INCLUDE, snapshotListener)
     }
 
     private fun processUser(
         user: FirestoreUser?,
         isFromCache: Boolean,
         userUUID: String,
+        updateCachedUserStatus: (DataStatus<FirestoreUser>) -> Unit,
+        updateCloudUserStatus: (DataStatus<FirestoreUser>) -> Unit,
     ) {
         // Cache
         if (user != null) {
@@ -180,9 +185,9 @@ class UserNetworkDataSource @Inject constructor(
             } else {
                 Timber.d("Firestore loaded ${userUUID.take(4)} user's data from Cache")
             }
-            _userStatus.update { DataStatus(data = user, isLoading = false) }
+            updateCachedUserStatus(DataStatus(data = user, isLoading = false))
         } else {
-            _userStatus.update { DataStatus(data = null, isLoading = false) }
+            updateCachedUserStatus(DataStatus(data = null, isLoading = false))
         }
 
         // Cloud
@@ -193,9 +198,9 @@ class UserNetworkDataSource @Inject constructor(
                 } else {
                     Timber.d("Firestore loaded ${userUUID.take(4)} user's data from Cloud")
                 }
-                _cloudUserStatus.update { DataStatus(data = user, isLoading = false) }
+                updateCloudUserStatus(DataStatus(data = user, isLoading = false))
             } else {
-                _cloudUserStatus.update { DataStatus(data = null, isLoading = false) }
+                updateCloudUserStatus(DataStatus(data = null, isLoading = false))
             }
         }
     }
@@ -225,7 +230,7 @@ class UserNetworkDataSource @Inject constructor(
 
     suspend fun deleteUserData(
         batch: WriteBatch,
-        onWriteFinished: () -> Unit = {}
+        onWriteFinished: () -> Unit = {},
     ) {
         _userReference.first { snapshot ->
             snapshot?.let {
